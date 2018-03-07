0bcf645d124d1fe2c31b2f5dcf96e5842ddfbb3a
刘晨 <7578916585@qq.com>
2018-03-07 14:52:53 +0800

    修改xml配置文件，去除控制台的log

--- 初学/入门例子/集群/Client/Program.cs
+++ 初学/入门例子/集群/Client/Program.cs
@@ -10,8 +10,9 @@ namespace Client
     class Program
     { 
         static void Main(string[] args)
-        { 
+        {
             //等待服务端启动完毕
+            Console.WriteLine("按任意键开始");
             Console.ReadKey();
             //然后我聪明的敲击了回车键
             Run();
@@ -24,13 +25,20 @@ namespace Client
             //初始化一个GrainClient
             GrainClient.Initialize("Client.xml");
 
-            Console.WriteLine("持久化开始");
+            //Console.WriteLine("持久化开始");
             //从silo处，获得了一个BasicGrain的接口
-            for (int i = 0; i < 10; i++)
+            int value = 1;
+            while (value != 0)
             {
-                var agrain = GrainClient.GrainFactory.GetGrain<IGrains.IClass1>(i+1);
-                var say = agrain.SayHello();
-                Console.WriteLine(say.Result);
+                Console.WriteLine("请问需要注册几个用户？");
+                string str = Console.ReadLine();
+                int num = int.Parse(str);
+                for (int i = 0; i < num; i++)
+                {
+                    var agrain = GrainClient.GrainFactory.GetGrain<IGrains.IClass1>(i + 1);
+                    var say = agrain.SayHello();
+                    Console.WriteLine(say.Result);
+                }
             }
             Console.WriteLine("按Enter键结束");
             Console.ReadKey();
