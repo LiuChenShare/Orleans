aeae9d16a97f78bdae82875f7d353d0510c61e10
村长 <757891685@qq.com>
2019-12-11 11:10:50 +0800

    定义Grain接口

--- netCore3.1/Hello World/IGrain/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace IGrain
-{
-    public class Class1
-    {
-    }
-}
---
+++ netCore3.1/Hello World/IGrain/IHello.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace IGrain
+{
+    public interface IHello : Orleans.IGrainWithIntegerKey
+    {
+        Task<string> SayHello(string greeting);
+    }
+}
